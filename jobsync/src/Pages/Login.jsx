9a114b767449fc53ded5f135a10df09e65726b05
Harnoor Kaur <103826473+harnoor144kaur@users.noreply.github.com>
2024-06-22 15:08:05 +0530

    Updated proxy url

--- jobsync/src/Pages/Login.jsx
+++ jobsync/src/Pages/Login.jsx
@@ -13,7 +13,7 @@ const Login = () => {
   const loginUser = async (e) => {
     e.preventDefault();
 
-    const response = await fetch("http://localhost:3000/api/user/login", {
+    const response = await fetch("https://job-sync.onrender.com/api/user/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password }),
